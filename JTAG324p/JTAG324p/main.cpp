4602ce534483c347574f37f172b8461cc229c0ab
slichtenheld <slichtenheld@ufl.edu>
2016-11-29 17:20:21 -0500

    added JTAG testing project

---
+++ JTAG324p/JTAG324p/main.cpp
@@ -0,0 +1,159 @@
+/*
+ * ATtiny841.cpp
+ *
+ * Created: 10/27/2016 7:02:27 PM
+ * Author : samuel
+ */ 
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "usartJTAG.h"
+#include "C:\Users\samuel\Documents\github\seniorDesign\myHeaderFiles\timer1.h"
+#include "C:\Users\samuel\Documents\github\seniorDesign\myHeaderFiles\messages.h"
+
+
+void dispense();
+void returnStatus();
+void invalidCommand();
+void stepperPortOutput();
+void fullRotation();
+
+
+//GLOBAL VARIABLES
+
+volatile unsigned char status = STATUSOK;
+//int step[] = {0b1100,0b0100, 0b0110,0b0010,0b0011,0b0001,0b1001, 0b1000}; // delay 8 ms
+//int fullStep[] = {0b1100,0b0110,0b0011,0b1001}; // delay 15 ms
+	//
+//unsigned char test[]= {0b0,0b1,0b10,0b100,0b1000,0b10000,0b100000,0b10000000};
+
+volatile uint8_t outstandingRqsts;
+volatile unsigned char msgRqst; // implement circular buffer?
+volatile int8_t msgStatus;	  // receive status
+
+volatile bool mode; 
+#define MPCM_MODE 1// Multi-processor Communication mode - only stores data in receive buffer if 9th bit is set (address word)
+#define DATA_MODE 0
+volatile const unsigned char slaveAddr = 'X'; // use X, Y, and Z
+
+int main(void)
+{
+	// initialize PORTB as outputs for debug purposes
+	DDRB = 0b111;
+	PORTB = 0b111;
+	//fullRotation();
+	//PORTB = 0;
+	//DDRA = 0b1111;
+	//stepperPortOutput();
+	/* Replace with your application code */
+	USART1_init();
+	USART1_receiveInterruptEnable();
+	sei();
+	
+	msgStatus = 0;	  // receive status
+	outstandingRqsts = 0;
+	msgRqst = 0;
+	mode = DATA_MODE;
+	
+	while(1) { // infinite loop checking for outstanding requests
+		
+		asm volatile("nop");
+		if (outstandingRqsts > 0) 
+		{ // if outstanding request, handle request
+			// CHECK IF BUSY --> SEND BUSY RESPONSE
+		asm volatile("nop");		
+ 			PORTB &= ~(1<<DDRB2);
+ 			switch (msgStatus){
+ 				case (FRAMEERROR):
+ 					//clearReceiveBuffer();
+ 					//PORTB &= ~(1<<DDRB2);
+ 					USART1_transmit(slaveFrameError, DATA_MSG);
+ 					break;
+ 				case (DATAOVERRUNERROR):
+ 					//clearReceiveBuffer();
+ 					//PORTB &= ~(1<<DDRB1);
+ 					USART1_transmit(slaveDataOverRunError, DATA_MSG);
+ 					break;
+ 				case (PARITYERROR):
+ 					//clearReceiveBuffer();
+ 					//PORTB &= ~(1<<DDRB0);
+ 					USART1_transmit(slaveParityError, DATA_MSG);
+ 					break;
+ 				default: // no receive error
+ 						//PORTB = 0b111;
+ 						//PORTB = 0;
+ 						switch(msgRqst){
+ 							//case (DISPENSE):
+ 								//fullRotation();
+ 								//USART1_transmit(DISPENSECOMPLETE, DATA_MSG);
+ 								//PORTB &= ~(1<<DDRB0);
+ 								//break;
+ 							//case (STATUS):
+ 								//USART1_transmit(STATUSOK, DATA_MSG);
+ 								//PORTB &= ~(1<<DDRB1);
+ 								//break;
+ 							default:
+ 								USART1_transmit(msgRqst, DATA_MSG);
+ 						}
+ 			}
+ 			outstandingRqsts = 0;
+		}
+	}
+}
+
+ISR(USART1_RX_vect) {
+	unsigned char msgTemp = 0;
+	volatile int8_t errorTemp = 0;
+	errorTemp = USART1_receive(&msgTemp);
+//	clearReceiveBuffer();
+	if (mode==MPCM_MODE)
+	{
+		// check for address match
+	
+		if (msgTemp==slaveAddr){ // otherwise, see if MCU is trying to talk to this slave
+			USART1_MPCM_off();
+			mode=DATA_MODE;
+		}		 
+		else { // if error message, reset receive buffer
+			clearReceiveBuffer();
+		}
+	}
+	else{ // normal data, actually care what it has to say
+		outstandingRqsts = 1;
+		msgStatus = errorTemp;
+		msgRqst = msgTemp;
+		
+		mode = MPCM_MODE; // go back to listening for address
+		USART1_MPCM_on();
+	}
+	
+}
+
+void stepperPortOutput(){
+	// initialize PORTA[3..0] as outputs to drive motor driver
+	DDRA |= 0b1111;
+}
+
+void stepperPortInput(){
+	DDRA &= ~(0b1111); //make inputs so don't draw power
+}
+
+void fullRotation(){
+	int fullStepB[] = {0b1100, 0b1001, 0b0011, 0b0110};
+	//full rotation function
+	stepperPortOutput(); // DDR* {1 for output pin, 0 for input pin}
+	uint16_t currentLocation = 0;
+	while(currentLocation < 512) {
+		for (uint8_t i = 0; i < 4; i++) {
+			PORTA = 0;
+			PORTA = fullStepB[i];
+			_delay_us(2000);
+		}
+		currentLocation++;
+	}
+	stepperPortInput();
+}
+
+
---
+++ JTAG324p/JTAG324p/usartJTAG.h
@@ -0,0 +1,132 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 11/11/2016 4:34:41 PM
+ *  Author: samuel
+ */ 
+
+
+#ifndef USART_H
+#define USART_H
+
+#define NINEBITMODE 1
+
+
+#define ADDRESS_MSG 1
+#define DATA_MSG 0
+
+	#define SLAVE 1
+
+
+#define BAUD 100
+#define BAUDRATE ((F_CPU)/(BAUD*16UL)-1)
+
+
+
+void USART1_transmitEnable();
+void USART1_transmitDisable();
+void USART1_receiveEnable();
+void USART1_receiveDisable();
+
+void USART1_MPCM_on();
+
+// UART1 RX Functions
+int8_t USART1_receive(unsigned char *message) {
+	
+	//wait for message to be received
+	//while(! (UCSR1A & (1 << RXC1)) ); not needed for interrupts
+	
+	int8_t errtemp = 0;
+	if (UCSR1A & (1 << FE1)) errtemp = -1; // check for frame error
+	else if (UCSR1A & (1 << DOR1)) errtemp = -2; // check for Data OverRun Error
+	else if (UCSR1A & (1 << UPE1)) errtemp = -3; // check for Parity Error
+		
+	*message = UDR1; //reading UDR1 should clear error flags, doesn't appear to actually do so
+	
+	return errtemp;
+}
+
+// reset receive buffer, for some reason if multiple missed reads occur timeout no longer functions (has to do with RXC1 bit)
+void clearReceiveBuffer(void) {
+	volatile unsigned char dummy = 0;
+	while ( UCSR1A & (1<<RXC1) ) dummy = UDR1;
+	USART1_receiveDisable();
+	USART1_receiveEnable();
+	asm volatile("nop");
+	dummy = UDR1;
+	asm volatile("nop");
+};
+
+// UART1 TX Functions
+void USART1_transmit(unsigned char data, bool transmitMode) { //IF ISSUES CHECK DEFINES FOR INIT_CLOCK ETC AT TOP OF FILE
+	
+	/* Wait for empty transmit buffer*/
+	while( !(UCSR1A & (1 << UDRE1)) ); 
+	
+	/* Copy 9th bit to TXB8, 9th bit is transmitMode */ 
+	UCSR1B &= ~(1<<TXB81); // clear TXB81 first
+	if ( transmitMode ) UCSR1B |= (1<<TXB81); // if address Mode, write a one to TXB81
+	
+	UDR1 = data;
+	
+	//The TXC Flag (UCSRnA.TXC)
+	//while( !(UCSR1A & (1 << TXC1)) ); // Wait until transmit finished - OPTIONAL
+	//UCSR1A |= (1 << TXC1);
+	
+}
+
+// UART1 Initialization
+void USART1_init(void) {
+	UBRR1H = (unsigned char)(BAUDRATE >> 8);
+	UBRR1L = (unsigned char)(BAUDRATE);
+	
+	UCSR1C = (1<<UMSEL10) | (3<<UCSZ10)| (1<<USBS1) | (3<<UPM10); // synchronous operation and 9-bit word, 2 stop bits, odd parity
+	UCSR1B |= (1<<UCSZ12) ; // NEED THIS FOR 9-BIT WORD, UCSZ1[0..2] SPLIT AMONG THESE 2 REGS
+	
+	/* if slave module, turn on multiMaster mode */
+	USART1_MPCM_on();
+	
+	// enable TX/RX pins
+	USART1_transmitEnable();
+	USART1_receiveEnable();
+}
+
+void USART1_MPCM_on(){
+	UCSR1A |= (1<<MPCM1);
+}
+void USART1_MPCM_off(){
+	UCSR1A &= ~(1<<MPCM1);
+}
+
+
+void USART1_transmitEnable(){
+	UCSR1B |= (1 << TXEN1) ;
+}
+void USART1_transmitDisable(){
+	UCSR1B &= ~(1 << TXEN1) ;
+}
+
+void USART1_receiveEnable(){
+	UCSR1B |= (1 << RXEN1);
+}
+void USART1_receiveDisable(){
+	UCSR1B &= ~(1 << RXEN1);
+}
+	
+void USART1_receiveInterruptEnable(){
+	// enable RX interrupt
+	UCSR1B |= (1 << RXCIE1);
+}
+
+void USART1_receiveInterruptDisable(){
+	UCSR1B &= ~(1 << RXCIE1);
+}
+
+void USART1_transmitInterruptEnable(){
+	UCSR1B |= (1 << TXCIE1);
+}
+
+
+
+
+#endif // USART_H
